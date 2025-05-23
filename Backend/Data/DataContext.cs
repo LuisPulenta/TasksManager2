7c00400d719d6d14fcfba02b936ed76d6db8d12d
Luis Núñez <luisalbertonu@gmail.com>
2025-05-23 19:09:25 -0300

    Create Database

---
+++ Backend/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Entities;
+
+namespace Backend.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options ):base(options) 
+        { 
+        }
+
+        public DbSet<MyTask> MyTasks { get; set; }
+    }
+}
